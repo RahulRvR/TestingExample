7e38400b47952bdb8d702dc7b429816bc8f0e201
Rahul RV <rahul.rv87@gmail.com>
2015-09-09 22:36:50 -0400

    Initial Commit

---
+++ app/src/main/java/com/example/testing/testingexample/Calculator.java
@@ -0,0 +1,23 @@
+package com.example.testing.testingexample;
+
+/**
+ * Created by rahulrv on 9/9/15.
+ */
+public class Calculator {
+
+    public double sum(double a, double b){
+        return 0;
+    }
+
+    public double subtract(double a, double b){
+        return 0;
+    }
+
+    public double divide(double a, double b){
+        return 0;
+    }
+
+    public double multiply(double a, double b){
+        return 0;
+    }
+}
